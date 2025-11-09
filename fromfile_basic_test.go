c56e797bbc443d569f7e0be84b421ea703bd5cf9
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:01 -0800

    tests

--- fromfile_basic_test.go
+++ fromfile_basic_test.go
@@ -1,17 +1,16 @@
 package perl_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/perl"
 )
 
 // This example demonstrates reading from a file instead of strings.NewReader
 func ExamplePerl_fromFile_basic() {
 	// cat testdata/text.txt | perl -pe 's/world/universe/'
-	yup.MustRun(
-		Perl(PrintLoop, Code("s/world/universe/"), yup.File("testdata/text.txt")),
+	gloo.MustRun(
+		Perl(PrintLoop, Code("s/world/universe/"), gloo.File("testdata/text.txt")),
 	)
 	// Output:
 	// hello universe
 }
-
